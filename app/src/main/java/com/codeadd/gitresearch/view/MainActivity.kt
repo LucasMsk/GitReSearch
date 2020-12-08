26d993191e21a54e733a58fedc24f7bb037039d8
LucMas <lukimssss@gmail.com>
2020-12-08 01:42:41 +0100

    UI for Search Fragment

--- app/src/main/java/com/codeadd/gitresearch/view/MainActivity.kt
+++ app/src/main/java/com/codeadd/gitresearch/view/MainActivity.kt
@@ -2,11 +2,17 @@ package com.codeadd.gitresearch.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.core.content.ContextCompat
 import com.codeadd.gitresearch.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (android.os.Build.VERSION.SDK_INT >= 23) {
+            window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+            window?.statusBarColor = ContextCompat.getColor(applicationContext, R.color.white)
+        }
     }
 }
